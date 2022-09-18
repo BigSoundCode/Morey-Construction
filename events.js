b15020b844def05f35263fecea05d44959d695cc
BigSoundCode <95320434+BigSoundCode@users.noreply.github.com>
2022-09-18 13:41:14 -0400

    Add files via upload

---
+++ events.js
@@ -0,0 +1,18 @@
+//resize header on scroll
+//window.onscroll = function() {scrollFunction()};
+//
+//function scrollFunction() {
+//  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//    document.getElementById("header").style.height = "100px";
+//  } 
+//  else {
+//    document.getElementById("header").style.height = "auto";
+//  }
+//} 
+//
+//$(window).resize(function() {
+//    var height = $('.header').height();
+//    $('main').css({
+//      'margin-top': height
+//    });
+//  }).trigger('resize');
